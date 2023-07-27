ae2eb15dde9c558ba7318999457d79d65c41648b
pmueller23 <63987897+pmueller23@users.noreply.github.com>
2023-07-27 18:00:59 -0500

    refactor header code. add javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Hi!, Welcome to My Portfolio Site");
+var hamburger = document.querySelector(".hamburger");
+var navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+$("a").click(function (e) {
+  $("html, body").animate(
+    {
+      scrollTop: $("#contact").offset().top,
+    },
+    1000
+  );
+});
